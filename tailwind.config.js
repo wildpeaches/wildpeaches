41d6961a0e98821671f7b04e24ef2550a87c9058
Jan De Wilde <jan@jandewilde.com>
2020-12-07 13:15:13 -0500

    TailwindCSS upgrade (#58)
    * TailwindCSS upgrade
    - removed purgeCSS as it is now included
    - removed autoprefixer in favor of postcss-preset-env
    - fixed a postCSS error (https://github.com/postcss/postcss/issues/1420#issuecomment-739413748)
    - renamed whitespace-no-wrap to whitespace-nowrap (tailwindCSS v2.0 change)
    
    * tailwind color and mediaquery tweaking

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,18 @@
+const colors = require("tailwindcss/colors");
+
 module.exports = {
   important: true,
+  purge: ['./www/**/*.html'],
   theme: {
     screens: {
       sm: '640px',
-      md: '768px',
-      lg: '1024px',
-      xl: '1280px',
-      xxl: '1680px'
+      md: '1024px',
+      lg: '1280px'
+    },
+    colors: {
+      gray: colors.blueGray,
+      blue: colors.blue,
+      white: colors.white,
     },
     listStyleType: {
       none: 'none',
